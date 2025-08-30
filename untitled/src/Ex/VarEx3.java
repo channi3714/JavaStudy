5d8bbd0357cf91bf6b23e043453a43bb757d06de
channi <dhckd3714@gmail.com>
2025-08-30 21:31:04 +0900

    Initial commit

---
+++ untitled/src/Ex/VarEx3.java
@@ -0,0 +1,10 @@
+package Ex;
+
+public class VarEx3 {
+    public static void main(String[] args) {
+        long longVar = 10000000000L;
+        System.out.println(longVar);
+        boolean boolVar = true;
+        System.out.println(boolVar);
+    }
+}
